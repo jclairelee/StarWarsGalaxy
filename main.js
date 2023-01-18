3ca516bcbb3be26161fadada562975efbd9e8fd0
Claire Lee <jclairelee@gmail.com>
2023-01-18 14:17:03 -0800

    Add Spaceship Movement

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ document.body.appendChild(canvas);
 //Load Images
 let bgImg,
   galaxyBG,
+  spaceship,
   spacecraft,
   spacecraft1,
   spacecraft2,
@@ -21,15 +22,18 @@ let bgImg,
   gameOverImg,
   bullet;
 
+let spaceshipX = canvas.width / 2 - 48;
+let spaceshipY = canvas.height - 48;
+
 const loadImg = () => {
   bgImg = new Image();
-  bgImg.src = "image/galaxyBG.webp";
+  bgImg.src = "image/galaxyBG.png";
 
   bullet = new Image();
-  bullet.src = "image/bullet.png";
+  bullet.src = "/image/bullet.png";
 
-  spaceshipImg = new Image();
-  spaceshipImg.src = "image/myShip.png";
+  spaceship = new Image();
+  spaceship.src = "image/myShip.png";
 
   gameOverImg = new Image();
   gameOverImg.src = "image/gameOver.jpeg";
@@ -49,3 +53,57 @@ const loadImg = () => {
   spacecraft4 = new Image();
   spacecraft4.src = "image/spacecraft4.png";
 };
+
+//keysPressed contains the kyes that user pressed
+let keysDown = {};
+const keyboardListener = () => {
+  document.addEventListener("keydown", (e) => {
+    keysDown[e.key] = true;
+    console.log(keysDown);
+  });
+  document.addEventListener("keyup", (e) => {
+    delete keysDown[e.key];
+    console.log(keysDown);
+  });
+};
+
+const update = () => {
+  if ("ArrowRight" in keysDown) {
+    //right
+    spaceshipX += 6;
+  }
+  if ("ArrowLeft" in keysDown) {
+    //left
+    spaceshipX -= 6;
+  }
+  if ("ArrowUp" in keysDown) {
+    //Up
+    spaceshipY -= 6;
+  }
+  if ("ArrowDown" in keysDown) {
+    //down
+    spaceshipY += 6;
+  }
+
+  if (spaceshipX <= 0) {
+    spaceshipX = 0;
+  }
+  if (spaceshipX >= canvas.width - 48) {
+    spaceshipX = canvas.width - 48;
+  }
+};
+const renderImg = () => {
+  ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(spaceship, spaceshipX, spaceshipY);
+};
+
+const main = () => {
+  update();
+  renderImg();
+  console.log("render image");
+  requestAnimationFrame(main);
+};
+
+loadImg();
+keyboardListener();
+main();
